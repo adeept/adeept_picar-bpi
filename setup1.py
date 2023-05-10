22e851bcc6c8da47ffd317d2f14c8963123ec548
Devin <support@adeept.com>
2023-05-10 16:44:45 +0800

    update

--- setup1.py
+++ setup1.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 # File name   : setup.py
 # Author      : Adeept
-# Date        : 2020/3/14
 
 import os
 import time
@@ -19,24 +18,6 @@ def replace_num(file,initial,new_num):
             newline += line
     with open(file,"w") as f:
         f.writelines(newline)
-
-
-commands_2 = [
-    "sudo apt-get -y install libqtgui4 libhdf5-dev libhdf5-serial-dev libatlas-base-dev libjasper-dev libqt4-test",
-    "sudo git clone https://github.com/oblique/create_ap",
-    "cd " + thisPath + "/create_ap && sudo make install",
-    "cd //home/pi/create_ap && sudo make install",
-    "sudo apt-get install -y util-linux procps hostapd iproute2 iw haveged dnsmasq"
-]
-
-mark_2 = 0
-for x in range(3):
-    for command in commands_2:
-        if os.system(command) != 0:
-            print("Error running installation step 2")
-            mark_2 = 1
-    if mark_2 == 0:
-        break
 
 try:
     replace_num("/boot/config.txt", '#dtparam=i2c_arm=on','dtparam=i2c_arm=on\nstart_x=1\n')
@@ -65,10 +46,7 @@ try: #fix conflict with onboard Raspberry Pi audio
         file_to_write.write("blacklist snd_bcm2835")
 except:
     pass
-try:
-    os.system("sudo cp -f //home/pi/adeept_rasptank/server/config.txt //etc/config.txt")
-except:
-    os.system("sudo cp -f "+ thisPath  +"/adeept_rasptank/server/config.txt //etc/config.txt")
-print('The program in Raspberry Pi has been installed, disconnected and restarted. \nYou can now power off the Raspberry Pi to install the camera and driver board (Robot HAT). \nAfter turning on again, the Raspberry Pi will automatically run the program to set the servos port signal to turn the servos to the middle position, which is convenient for mechanical assembly.')
+
+print('The program in Banana Pi has been installed. \nAfter turning on again, the Banana Pi will automatically run the program to set the servos port signal to turn the servos to the middle position, which is convenient for mechanical assembly.')
 print('restarting...')
 os.system("sudo reboot")
